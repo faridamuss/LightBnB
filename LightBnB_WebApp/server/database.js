9ed37e19ac77b50fdea0786897bef7f4745c1a5d
Farida <faridamus@protonmail.com>
2021-11-25 23:56:32 +0000

    Update functions to use the lightbnb database with SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -18,16 +18,13 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool
+  .query(`SELECT * FROM users WHERE email=$1`,[email])
+  .then((result) => {
+    return result.rows[0]}
+    )
+  .catch((err) => console.log(err))
+
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,8 +34,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id=$1`,[id])
+  .then((result) => {
+    return result.rows[0]}
+    )
+  .catch((err) => console.log(err))
 }
+
 exports.getUserWithId = getUserWithId;
 
 /**
@@ -47,10 +50,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+  .then((result) => {
+    return result.rows[0]}
+    )
+  .catch((err) => console.log(err))
 }
 exports.addUser = addUser;
 
@@ -76,12 +81,6 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
 return pool
      .query (`SELECT * FROM properties LIMIT $1`, [limit])
      .then((result) => result.rows)
