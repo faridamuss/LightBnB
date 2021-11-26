609c5acf9980918d6ee67d17bc8aa4a08a26bd9d
Farida <faridamus@protonmail.com>
2021-11-26 01:39:49 +0000

    Update addProperty function to save a new property to the properties table

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -129,9 +129,47 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id, 
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  let queryString = `
+  INSERT INTO properties (
+    owner_id, 
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  )
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`;
+
+  return pool
+  .query(queryString, queryParams)
+  .then((result) => result.rows)
+  .catch((err) => console.log(err.message));
 }
 exports.addProperty = addProperty;
+
