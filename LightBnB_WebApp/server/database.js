29b5515e06d6c4236796b863112e064e1a1feea7
Farida <faridamus@protonmail.com>
2021-11-26 01:27:14 +0000

    Update functions after testing

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -19,11 +19,9 @@ const users = require('./json/users.json');
  */
 const getUserWithEmail = function(email) {
   return pool
-  .query(`SELECT * FROM users WHERE email=$1`,[email])
-  .then((result) => {
-    return result.rows[0]}
-    )
-  .catch((err) => console.log(err))
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then((result) => result.rows[0]) //NULL if user does not exist
+  .catch((err) => console.log(err.message));
 
 }
 exports.getUserWithEmail = getUserWithEmail;
@@ -35,11 +33,9 @@ exports.getUserWithEmail = getUserWithEmail;
  */
 const getUserWithId = function(id) {
   return pool
-  .query(`SELECT * FROM users WHERE id=$1`,[id])
-  .then((result) => {
-    return result.rows[0]}
-    )
-  .catch((err) => console.log(err))
+  .query(`SELECT * FROM users WHERE users.id = $1`, [id])
+  .then((result) => result.rows[0]) 
+  .catch((err) => console.log(err.message));
 }
 
 exports.getUserWithId = getUserWithId;
@@ -51,11 +47,11 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser =  function(user) {
   return pool
-  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
-  .then((result) => {
-    return result.rows[0]}
-    )
-  .catch((err) => console.log(err))
+  .query(`INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3) 
+  RETURNING *`, [user.name, user.email, user.password])
+  .then((result) => result.rows[0]) 
+  .catch((err) => console.log(err.message));
 }
 exports.addUser = addUser;
 
@@ -74,11 +70,9 @@ const getAllReservations = function(guest_id, limit = 10) {
     )
   .catch((err) => console.log(err))
 }
-
 exports.getAllReservations = getAllReservations;
 
 /// Properties
-
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
