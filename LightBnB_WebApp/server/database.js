57a29a0f55494576ba2e25fb4115112bb9ae0af3
Farida <faridamus@protonmail.com>
2021-11-26 00:01:18 +0000

    Update the getAllReservations function to use lightbnb database with SQL queries

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -67,8 +67,14 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT * FROM reservations JOIN properties ON properties.id = property_id WHERE guest_id=$1 LIMIT $2`, [guest_id, limit])
+  .then((result) => {
+    return result.rows}
+    )
+  .catch((err) => console.log(err))
 }
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
